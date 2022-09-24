73f9317a3e778347cc43a1433423f7f715683b58
asep38 <asepzarkasihnoor38@gmail.com>
2022-09-24 16:19:07 +0700

    view and hide pssdw

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container"),
   showhidePW = document.querySelectorAll(".HidePiw"),
-  PWfield = document.querySelector(".password"),
+  PWfield = document.querySelectorAll(".password"),
   SignUp = document.querySelector(".sign-up-link"),
   Login = document.querySelector(".login-link");
 
@@ -10,4 +10,25 @@ SignUp.addEventListener("click", () => {
 });
 Login.addEventListener("click", () => {
   container.classList.remove("active");
+});
+
+// hiden and view passwd
+
+showhidePW.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    PWfield.forEach((PWfield) => {
+      if (PWfield.type === "password") {
+        PWfield.type = "text";
+
+        showhidePW.forEach((icon) => {
+          icon.classList.replace("uil-eye-slash", "uil-eye");
+        });
+      } else {
+        PWfield.type = "password";
+        showhidePW.forEach((icon) => {
+          icon.classList.replace("uil-eye", "uil-eye-slash");
+        });
+      }
+    });
+  });
 });
